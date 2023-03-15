38b3083c19874f0a4a89024f01e5a92891879787
chicken10dys <chicken@10dys.ca>
2023-03-14 22:26:42 -0400

    update

--- jstest/main.js
+++ jstest/main.js
@@ -2,6 +2,6 @@ console.log("this js is running (main.js)")
 var runButton = document.getElementById("run");
 runButton.addEventListener("click", clicked);
 function clicked() {
+    runButton.innerHTML = "YAY :)"
     console.log("clicked");
-    alert("Button clicked");
 }
